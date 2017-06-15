4e8aab1cf855ab324b9a34d4b22b6a8004699ee4
Thys Giaccone <dofawaza@hotmail.fr>
2017-06-15 15:07:02 +0200

    Create lvl_gen.py

--- lvl_gen.py
+++ lvl_gen.py
@@ -9,9 +9,32 @@ import os
 
 np.set_printoptions(threshold=np.nan)
 N_bricks = 8*9
-p_ordered = np.hstack((1*np.ones(N_bricks//2), 5*np.ones(N_bricks//2)))
-np.random.seed(42)
-p_randomized = np.random.permutation(p_ordered).reshape((9,8))
 
-with open(os.path.join('lvl.npy'), 'w') as data:
-    data.write('%s' %np.array2string(np.array(p_randomized).astype(int), separator=', '))
+x = 1
+while x < 7:
+    """ Creation of lvl 1 """
+    if x == 1:
+        p_ordered = np.ones(N_bricks)
+    """ Creation of lvl 2 """
+    if x == 2:
+        p_ordered = np.hstack((1*np.ones(N_bricks//2), 5*np.ones(N_bricks//2)))
+    """ Creation of lvl 3 """
+    if x == 3:
+        p_ordered = np.hstack((1*np.ones(N_bricks//4), 5*np.ones(N_bricks//4), 3*np.ones(N_bricks//2)))
+    """ Creation of lvl 4 """
+    if x == 4:
+        p_ordered = np.hstack((2*np.ones(N_bricks//2), 4*np.ones(N_bricks//2)))
+    """ Creation of lvl 5 """
+    if x == 5:
+        p_ordered = np.hstack((1*np.ones(N_bricks//4), 2*np.ones(N_bricks//4), 3*np.ones(N_bricks//4), 4*np.ones(N_bricks//4)))
+    """ Creation of lvl 6 """
+    if x == 6:
+        p_ordered = np.hstack((1*np.ones(N_bricks//5), 2*np.ones(N_bricks//5), 3*np.ones(N_bricks//5+2), 4*np.ones(N_bricks//5), 5*np.ones(N_bricks//5)))
+
+    x += 1
+
+    np.random.seed(42)
+    p_randomized = np.random.permutation(p_ordered).reshape((8,9))
+
+    with open(os.path.join('lvl.npy'), 'a') as data:
+        data.write('%s\n\n' %np.array2string(np.array(p_randomized).astype(int), separator=', '))
